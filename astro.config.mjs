e37d8d8d1c3a34872232bb5bf4bf07c817fe5a1d
Tara Bird <tlerias@gmail.com>
2026-05-15 20:57:28 -0400

    feat: switch to GitHub Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://tarabird.com',
   output: 'static',
-  adapter: vercel(),
   integrations: [
     mdx(),
     sitemap(),
